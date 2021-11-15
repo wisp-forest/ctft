c633ef1e883b498dcc5d12617f755775844980e8
chyzman <chyzalt@gmail.com>
2021-11-15 12:15:51 -0500

    mixin fixin

--- src/main/java/com/chyzman/mixin/CustomFishingItemFix.java
+++ src/main/java/com/chyzman/mixin/CustomFishingItemFix.java
@@ -6,7 +6,6 @@ import net.minecraft.world.item.FishingRodItem;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
